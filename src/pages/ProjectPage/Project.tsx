59ab1a5a5af15f0f038811e6252cd2e06f488444
wowba <leeyuwk54@gmail.com>
2023-10-13 17:12:46 +0900

    feat : project.tsx 테스트 코드 작성

--- src/pages/ProjectPage/Project.tsx
+++ src/pages/ProjectPage/Project.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const Btn = styled.button`
+  margin: 10px;
+  border: 1px solid black;
+  background-color: beige;
+`;
 
 export default function Project() {
-  return <div>Project</div>;
+  const location = useLocation();
+
+  const handleCanbanCLick = (url: string) => {
+    window.history.pushState(null, "", location.pathname + url);
+  };
+
+  const handleTodoCLick = (url: string) => {
+    window.history.pushState(null, "", location.pathname + url);
+  };
+
+  return (
+    <>
+      <div>Project</div>
+      <Btn type="button" onClick={() => handleCanbanCLick("/canbanID")}>
+        canban
+      </Btn>
+      <Btn type="button" onClick={() => handleTodoCLick("/todoID")}>
+        todo
+      </Btn>
+    </>
+  );
 }
