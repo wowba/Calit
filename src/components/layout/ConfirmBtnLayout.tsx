43bceefb8b469185cb095d2929f6c85bf22f0d1f
SOL-MI <ovo10203@gmail.com>
2023-10-16 18:30:12 +0900

    feat : 초대링크로 입장 버튼 기본 동작 구현

---
+++ src/components/layout/ConfirmBtnLayout.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const ConfirmBtn = styled.button`
+  color: white;
+  background-color: #ee6a6a;
+  height: 38px;
+  width: 193px;
+  border-radius: 7px;
+`;
+export default ConfirmBtn;
--- src/pages/ProjectListPage/ProjectList.tsx
+++ src/pages/ProjectListPage/ProjectList.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import ConfirmBtn from "../../components/layout/ConfirmBtnLayout";
 
 export default function ProjectList() {
-  return <div>ProjectList</div>;
+  const [isClicked, setIsClicked] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const handleClick = () => {
+    if (isClicked) {
+      window.location.href = inputValue;
+      setInputValue("");
+    }
+    setIsClicked(!isClicked);
+  };
+  return (
+    <div>
+      <h1>ProjectList</h1>
+      <div>
+        <ConfirmBtn type="button" onClick={handleClick}>
+          초대링크로 입장
+        </ConfirmBtn>
+        {isClicked ? (
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="링크를 입력해주세요"
+          />
+        ) : null}
+      </div>
+    </div>
+  );
 }
