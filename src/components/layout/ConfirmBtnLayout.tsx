3432b0ce37dbf92b952a6a254715c057e2855a34
SOL-MI <ovo10203@gmail.com>
2023-10-17 14:27:28 +0900

    design : 버튼 width 임의 조절 가능하게 수정

--- src/components/layout/ConfirmBtnLayout.tsx
+++ src/components/layout/ConfirmBtnLayout.tsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
-const ConfirmBtn = styled.button`
+interface Props {
+  dynamicWidth?: string;
+}
+
+const ConfirmBtn = styled.button<Props>`
   color: white;
   background-color: #ee6a6a;
   height: 38px;
-  width: 193px;
+  width: ${(props) => (props.dynamicWidth ? props.dynamicWidth : "193px")};
   border-radius: 7px;
 `;
 export default ConfirmBtn;
