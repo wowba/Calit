c92b25eff3f50ecce140b8b76531eeeb02f9f158
applevalley <skatkd7571@naver.com>
2023-10-19 18:57:39 +0900

    design: 헤더 높이 고정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { styled } from "styled-components";
+import ShrikhandRegular from "../assets/fonts/Shrikhand-Regular.ttf";
+
+const HeaderLayout = styled.div`
+  height: 9vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0rem 1rem 0rem 1rem;
+  border-bottom: 1px solid #000000;
+`;
+
+const HeaderLogoParagraph = styled.p`
+  @font-face {
+    font-family: "ShrikhandRegular";
+    src: local("ShrikhandRegular"), url(${ShrikhandRegular});
+  }
+
+  font-family: "ShrikhandRegular";
+  font-weight: 500;
+  font-size: 2rem;
+`;
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <HeaderLayout>
+      <HeaderLogoParagraph>Calit!</HeaderLogoParagraph>
+    </HeaderLayout>
+  );
 }
