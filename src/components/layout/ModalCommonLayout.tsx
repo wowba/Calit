78f37d98b0814cab90b28f10671baa5f524edd1f
applevalley <skatkd7571@naver.com>
2023-10-23 15:45:04 +0900

    feat: 헤더 모달 컴포넌트 연결

--- src/components/layout/ModalCommonLayout.tsx
+++ src/components/layout/ModalCommonLayout.tsx
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useState, useEffect, ReactElement } from "react";
+import { styled } from "styled-components";
 
-export default function ModalCommon() {
-  return <div>ModalCommon</div>;
+const ModalLayout = styled.div`
+  height: 100%;
+`;
+
+const ModalBox = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+const ModalArea = styled.div`
+  width: 20rem;
+  height: 20rem;
+  z-index: 999;
+  position: absolute;
+  right: 0;
+  top: 4rem;
+  border-radius: 7px;
+  box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.3);
+
+  &.isHide {
+    display: none;
+  }
+
+  &.isShow {
+    display: block;
+  }
+`;
+
+interface ModalInfo {
+  modalSelected: number;
+  modalIndex: number;
+  children: ReactElement;
+}
+
+export default function ModalCommon(name: ModalInfo) {
+  const { modalIndex, modalSelected, children } = name;
+  const [activeIndex, setActiveIndex] = useState<number>();
+
+  const handleClick = () => {
+    if (activeIndex === modalIndex) {
+      // 같은 모달 아이콘 클릭시 기본 값 -1 부여를 통한 모달 닫힘 처리
+      setActiveIndex(-1);
+    }
+  };
+
+  useEffect(() => {
+    setActiveIndex(modalSelected);
+  }, [modalSelected]);
+
+  return (
+    <ModalLayout>
+      <ModalBox onClick={() => handleClick()}>
+        <>
+          <img src={children.props.children[0]} alt="modalIcon" />
+          {activeIndex === modalIndex ? (
+            <ModalArea className="isShow">
+              {children.props.children[1]}
+            </ModalArea>
+          ) : null}
+        </>
+      </ModalBox>
+    </ModalLayout>
+  );
 }
