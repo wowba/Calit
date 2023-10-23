d41d54702404d02e82f0a6d7abe28ac0d7ac14ea
SOL-MI <ovo10203@gmail.com>
2023-10-23 18:31:49 +0900

    fix : eslint 에러 수정

--- src/pages/ProjectListPage/ProjectIconContainer.tsx
+++ src/pages/ProjectListPage/ProjectIconContainer.tsx
@@ -132,12 +132,8 @@ export default function ProjectIconContainer({
 
   // 링크 복사 기능
   const handleCopyClipBoard = async (id: string) => {
-    try {
-      //   await navigator.clipboard.writeText(`calit-2f888.web.app/${id}`);
-      await navigator.clipboard.writeText(`localhost:3000/${id}`);
-    } catch (err) {
-      console.log(err);
-    }
+    //   await navigator.clipboard.writeText(`calit-2f888.web.app/${id}`);
+    await navigator.clipboard.writeText(`localhost:3000/${id}`);
   };
 
   return (
