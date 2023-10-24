e62f63af6d160fc1ec92626e2b38126202e18c78
applevalley <skatkd7571@naver.com>
2023-10-24 11:51:18 +0900

    feat: 모달 컴포넌트 클릭시 이벤트 버블링 수정

--- src/components/layout/ModalCommonLayout.tsx
+++ src/components/layout/ModalCommonLayout.tsx
@@ -1,6 +1,17 @@
 import React, { useState, useEffect, ReactElement } from "react";
 import { styled } from "styled-components";
 
+interface Props {
+  $dynamicWidth?: string;
+  $dynamicHeight?: string;
+}
+
+interface ModalInfo {
+  modalSelected: number;
+  modalIndex: number;
+  children: ReactElement;
+}
+
 const ModalLayout = styled.div`
   height: 100%;
 `;
@@ -12,9 +23,9 @@ const ModalBox = styled.div`
   position: relative;
 `;
 
-export const ModalArea = styled.div`
-  width: 20rem;
-  height: 20rem;
+export const ModalArea = styled.div<Props>`
+  width: ${(props) => (props.$dynamicWidth ? props.$dynamicWidth : "20rem")};
+  height: ${(props) => (props.$dynamicHeight ? props.$dynamicHeight : "20rem")};
   z-index: 999;
   position: absolute;
   right: 0;
@@ -23,19 +34,13 @@ export const ModalArea = styled.div`
   box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.3);
 `;
 
-interface ModalInfo {
-  modalSelected: number;
-  modalIndex: number;
-  children: ReactElement;
-}
-
 export default function ModalCommon(name: ModalInfo) {
   const { modalIndex, modalSelected, children } = name;
   const [activeIndex, setActiveIndex] = useState<number>();
 
   const handleClick = () => {
     if (activeIndex === modalIndex) {
-      // 같은 모달 아이콘 클릭시 기본 값 -1 부여를 통한 모달 닫힘 처리
+    // 같은 모달 아이콘 클릭시 기본 값 -1 부여를 통한 모달 닫힘 처리
       setActiveIndex(-1);
     } else {
       setActiveIndex(modalSelected);
