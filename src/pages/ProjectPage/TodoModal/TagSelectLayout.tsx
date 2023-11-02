48374624e012161afb6ad77096b2b18ec5dc36e5
SOL-MI <ovo10203@gmail.com>
2023-11-02 18:10:41 +0900

    feat : tag 생성, 변경 및 db 업데이트

---
+++ src/pages/ProjectPage/TodoModal/TagSelectLayout.tsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import CreatableSelect from "react-select/creatable";
+import { serverTimestamp, updateDoc } from "firebase/firestore";
+import { useRecoilValue } from "recoil";
+import kanbanState from "../../../recoil/atoms/kanban/kanbanState";
+
+interface Props {
+  $dynamicBg: string;
+}
+const TagContainer = styled.div<Props>`
+  display: inline-block;
+  background-color: ${(props) =>
+    props.$dynamicBg ? props.$dynamicBg : "#ffffff"};
+  border-radius: 10px;
+  white-space: nowrap;
+  padding: 2px 10px;
+`;
+
+export default function TagSelectLayout({
+  kanbanId,
+  kanbanRef,
+  todoRef,
+  todoDataState,
+}: any) {
+  const kanbanDataState = useRecoilValue(kanbanState);
+  const data = [...kanbanDataState];
+  const targetKanbanData = data.filter((item) => item[0] === kanbanId);
+  const tagData = targetKanbanData[0][1].tag_list;
+  const [optionList, setOptionList] = useState(tagData);
+  const [tagList, setTagList] = useState(todoDataState.todoData.todo_tag_list);
+
+  const [isTagListModified, setIsTagListModified] = useState(false);
+  const [isOptionListModified, setIsOptionListModified] = useState(false);
+
+  function getNewOptionData(inputValue: any, optionLabel: any) {
+    return {
+      label: optionLabel,
+      value: inputValue,
+      color: "#fff229",
+    };
+  }
+  const handleSelectChange = async (newValue: any, actionMeta: any) => {
+    // option 목록 업데이트 (중복 금지)
+    if (actionMeta.action === "create-option") {
+      const resultValue = newValue.filter(
+        (item: any) =>
+          !optionList.some((list: any) => list.value === item.value),
+      );
+      setOptionList([...optionList, ...resultValue]);
+      if (resultValue) {
+        setIsOptionListModified(true);
+      }
+    }
+    setTagList(newValue);
+    setIsTagListModified(true);
+  };
+
+  // firestore : kanban의 tag_list db 업데이트
+  useEffect(() => {
+    const updateOptionList = async () => {
+      await updateDoc(kanbanRef, {
+        tag_list: optionList,
+        modified_date: serverTimestamp(),
+      });
+    };
+    if (isOptionListModified) {
+      updateOptionList();
+      setIsOptionListModified(false);
+    }
+  }, [optionList]);
+
+  // firestore : todo의 todo_tag_list db 업데이트
+  useEffect(() => {
+    const updateTodoTagList = async () => {
+      await updateDoc(todoRef, {
+        todo_tag_list: tagList,
+        modified_date: serverTimestamp(),
+      });
+    };
+    if (isTagListModified) {
+      updateTodoTagList();
+    }
+    setIsTagListModified(false);
+  }, [tagList]);
+
+  return (
+    <CreatableSelect
+      closeMenuOnSelect={false}
+      isMulti
+      options={optionList}
+      // eslint-disable-next-line react/no-unstable-nested-components
+      formatOptionLabel={(option: any) => (
+        <TagContainer $dynamicBg={option.color}>{option.label}</TagContainer>
+      )}
+      value={tagList}
+      // eslint-disable-next-line react/jsx-no-bind
+      getNewOptionData={getNewOptionData}
+      onChange={handleSelectChange}
+      styles={{
+        multiValue: (baseStyles) => ({
+          ...baseStyles,
+          padding: "0",
+          margin: "0",
+          border: "1px solid black",
+          borderRadius: "0.5rem",
+          backgroundColor: "transparent",
+        }),
+        multiValueRemove: (baseStyles) => ({
+          ...baseStyles,
+          borderRadius: "0.5rem",
+        }),
+        control: (baseStyles) => ({
+          ...baseStyles,
+          width: "auto",
+          transition: "all 0.3s",
+          boxShadow: "none",
+          border: "1px solid transparent",
+          ":hover": { border: "1px solid black" },
+          ":focus": { border: "1px solid black" },
+        }),
+        valueContainer: (baseStyles) => ({
+          ...baseStyles,
+          paddingRight: "0",
+        }),
+      }}
+    />
+  );
+}
