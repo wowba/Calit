ac21cc07198cf079dcae460823e8b25c7b2cc131
applevalley <skatkd7571@naver.com>
2023-11-08 16:06:22 +0900

    refactor: 헤더 모달 바깥 선택시 모달 닫히게 수정

--- src/components/layout/ModalCommonLayout.tsx
+++ src/components/layout/ModalCommonLayout.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, ReactElement } from "react";
+import React, {
+  useRef,
+  useState,
+  useEffect,
+  ReactElement,
+  BaseSyntheticEvent,
+} from "react";
 import { styled } from "styled-components";
 
 interface Props {
@@ -45,7 +51,11 @@ export default function ModalCommon(name: ModalInfo) {
   const { modalIndex, modalSelected, children } = name;
   const [activeIndex, setActiveIndex] = useState<number>();
 
-  const handleClick = () => {
+  useEffect(() => {
+    setActiveIndex(modalSelected);
+  }, [modalSelected]);
+
+  const handleClickBox = () => {
     if (activeIndex === modalIndex) {
       // 같은 모달 아이콘 클릭시 기본 값 -1 부여를 통한 모달 닫힘 처리
       setActiveIndex(-1);
@@ -54,13 +64,38 @@ export default function ModalCommon(name: ModalInfo) {
     }
   };
 
-  useEffect(() => {
-    setActiveIndex(modalSelected);
-  }, [modalSelected]);
+  const handleOutsideClick = () => {
+    // 현재 활성화된 모달의 바깥 영역을 클릭한 경우, 기본 값 -1 부여를 통한 모달 닫힘 처리
+    setActiveIndex(-1);
+  };
+
+  const useOutsideClick = (callback: () => void) => {
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      const handleClick = (event: BaseSyntheticEvent | MouseEvent) => {
+        // ref가 존재하고, ref 안에 클릭한 부분이 없을 때에만(ref의 바깥쪽을 눌렀을 때) 동작
+        if (ref.current && !ref.current.contains(event.target)) {
+          callback();
+        }
+      };
+
+      document.addEventListener("click", handleClick);
+
+      return () => {
+        document.removeEventListener("click", handleClick);
+      };
+    }, [callback, ref]);
+    return ref;
+  };
+
+  //
+  const ref = useOutsideClick(handleOutsideClick);
 
   return (
-    <ModalLayout>
-      <ModalBox onClick={() => handleClick()}>
+    <ModalLayout className="modallayout">
+      {/* ref 속성에는 커스텀 훅 useOutsideClick으로부터 반환받은 ref가 들어감 */}
+      <ModalBox onClick={handleClickBox} className="modalbox" ref={ref}>
         {children.props.children[0]}
         {activeIndex === modalIndex ? children.props.children[1] : null}
       </ModalBox>
