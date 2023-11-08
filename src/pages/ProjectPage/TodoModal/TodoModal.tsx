14d97fffa312ad6bed5016c9308b9841af664868
SOL-MI <ovo10203@gmail.com>
2023-11-08 16:01:05 +0900

    fix : eslint 주석 제거

--- src/pages/ProjectPage/TodoModal/TodoModal.tsx
+++ src/pages/ProjectPage/TodoModal/TodoModal.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
