cff3327262d57a24dd7ac93fe4ba8985e785c81e
applevalley <skatkd7571@naver.com>
2023-11-09 18:08:11 +0900

    feat: 튜토리얼 모달 pagination 동작 중간 저장

--- src/components/modal/TutorialModal.tsx
+++ src/components/modal/TutorialModal.tsx
@@ -1,24 +1,112 @@
-import React from "react";
-// import { styled } from "styled-components";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from "react";
+import { styled } from "styled-components";
 import { ModalArea, ModalTitle } from "../layout/ModalCommonLayout";
 
 import TutorialText from "./TutorialText";
+
+const TutorialTextLayout = styled.div``;
+const TutorialTextContent = styled.div`
+  margin-bottom: 10px;
+  white-space: pre-line;
+`;
+const TutorialTextParagraph = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const TUTORIAL_LIST_TEXT = [
+  { key: "Calit?", content: "세상에서 가장 쉬운 협업 툴 Calit!" },
+  {
+    key: "프로젝트 만들기",
+    content: "하단의 + 버튼을 통해 프로젝트 추가가 가능합니다.",
+  },
+  {
+    key: "프로젝트 공유하기",
+    content: `프로젝트 카드 우측 상단의 클립보드 아이콘을 선택해 해당 프로젝트의 주소를 공유할 수 있습니다.`,
+  },
+  {
+    key: "프로젝트 삭제하기",
+    content:
+      "자신이 생성한 프로젝트인 경우, 프로젝트 카드 우측 상단의 톱니바퀴 아이콘을 선택해 프로젝트의 배경 이미지를 변경하거나, 프로젝트를 삭제할 수 있습니다.",
+  },
+  {
+    key: "프로젝트 참여하기",
+    content:
+      "공유받은 링크를 주소창에 바로 입력하거나, 초대링크로 입장 버튼을 클릭한 뒤 붙여넣어 프로젝트에 참여할 수 있습니다.",
+  },
+];
+
+const TUTORIAL_PROJECT_TEXT = [
+  {
+    key: "프로젝트 멤버 모달",
+    content: `프로젝트에 참여중인 사용자들의 정보를 확인할 수 있습니다.
+      초대하기 버튼을 선택해 프로젝트에 새로운 사용자를 초대할 수 있습니다.
+      프로젝트를 생성한 사용자는 내보내기 버튼을 통해 사용자를 프로젝트에서 내보낼 수 있습니다.`,
+  },
+  {
+    key: "북마크 모달",
+    content: `URL을 입력해 북마크를 등록할 수 있습니다.
+      북마크는 프로젝트 단위로 공유되기 때문에, 프로젝트에 참여중인 모든 사용자가 확인하고 추가하거나 제거할 수 있습니다.`,
+  },
+  {
+    key: "프로필 모달",
+    content:
+      "프로필 이미지, 사용자 이름, 사용자 소개 글의 편집 및 로그아웃이 가능합니다.",
+  },
+];
 
 const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
   // 모달 컴포넌트 영역 클릭시 클릭 이벤트가 부모로 전달되어 컴포넌트가 닫히는 현상 수정
   event.stopPropagation();
 };
 
-// 프로젝트 리스트 페이지에서의 튜토리얼, 캘린더에서의 튜토리얼
-// 소개, 입장, 생성, 삭제
-// 멤버모달, 북마크모달, 프로필모달
 export default function Tutorial() {
   const projectId = window.location.pathname.substring(1);
-  const target = !projectId ? "List" : "Project";
+  const tutorialText = !projectId ? "List" : "Project";
+  const tutorialTarget = !projectId
+    ? TUTORIAL_LIST_TEXT
+    : TUTORIAL_PROJECT_TEXT;
+
+  const [text, setText] = useState<any>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [textPerPage, setTextPerPage] = useState(1);
+
+  useEffect(() => {
+    setText(tutorialTarget);
+  }, []);
+
   return (
     <ModalArea $dynamicWidth="" $dynamicHeight="auto" onClick={handleClick}>
-      <ModalTitle>{`${target} Tutorial`}</ModalTitle>
-      <TutorialText tutorialTarget={target} />
+      <ModalTitle>{`${tutorialText} Tutorial`}</ModalTitle>
+      <TutorialTextLayout>
+        {text.map((singleElement: any) => (
+          <TutorialTextContent key={singleElement.key}>
+            <TutorialTextParagraph>{singleElement.key}</TutorialTextParagraph>
+            {singleElement.content}
+          </TutorialTextContent>
+        ))}
+      </TutorialTextLayout>
+
+      <TutorialText
+        textPerPage={textPerPage}
+        totalText={text.length}
+        paginate={setCurrentPage}
+      />
     </ModalArea>
   );
 }
+
+// const Posts = ({ posts, loading }) => {
+//   return (
+//     <>
+//       {loading && <div> loading... </div>}
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.id}>{post.title}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+// export default Posts;
--- src/components/modal/TutorialText.tsx
+++ src/components/modal/TutorialText.tsx
@@ -1,72 +1,74 @@
 import React from "react";
 import { styled } from "styled-components";
 
-const TutorialTextLayout = styled.div``;
-const TutorialTextContent = styled.div`
-  margin-bottom: 10px;
-  white-space: pre-line;
+const TutorialUl = styled.ul`
+  float: left;
+  list-style: none;
+  text-align: center;
+  border-radius: 3px;
+  color: white;
+  padding: 1px;
+  border-top: 3px solid #186ead;
+  border-bottom: 3px solid #186ead;
+  background-color: rgba(0, 0, 0, 0.4);
 `;
-const TutorialTextParagraph = styled.p`
-  font-size: 1.5rem;
-  font-weight: bold;
+
+const TutorialLi = styled.li`
+  display: inline-block;
+  font-size: 17px;
+  font-weight: 600;
+  padding: 5px;
+  border-radius: 5px;
+  width: 25px;
+  &:hover {
+    cursor: pointer;
+    color: white;
+    background-color: #263a6c;
+  }
+  &:focus::after {
+    color: white;
+    background-color: #263a6c;
+  }
+`;
+
+const TutorialSpan = styled.span`
+  &:hover::after,
+  &:focus::after {
+    border-radius: 100%;
+    color: white;
+    background-color: #263a6c;
+  }
 `;
 
 interface Props {
-  tutorialTarget: string;
+  textPerPage: any;
+  totalText: any;
+  paginate: any;
 }
 
-const TUTORIAL_LIST_TEXT = [
-  { key: "Calit?", content: "세상에서 가장 쉬운 협업 툴 Calit!" },
-  {
-    key: "프로젝트 만들기",
-    content: "하단의 + 버튼을 통해 프로젝트 추가가 가능합니다.",
-  },
-  {
-    key: "프로젝트 공유하기",
-    content: `프로젝트 카드 우측 상단의 클립보드 아이콘을 선택해 해당 프로젝트의 주소를 공유할 수 있습니다.`,
-  },
-  {
-    key: "프로젝트 삭제하기",
-    content:
-      "자신이 생성한 프로젝트인 경우, 프로젝트 카드 우측 상단의 톱니바퀴 아이콘을 선택해 프로젝트의 배경 이미지를 변경하거나, 프로젝트를 삭제할 수 있습니다.",
-  },
-  {
-    key: "프로젝트 참여하기",
-    content:
-      "공유받은 링크를 주소창에 바로 입력하거나, 초대링크로 입장 버튼을 클릭한 뒤 붙여넣어 프로젝트에 참여할 수 있습니다.",
-  },
-];
-
-const TUTORIAL_PROJECT_TEXT = [
-  {
-    key: "프로젝트 멤버 모달",
-    content: `프로젝트에 참여중인 사용자들의 정보를 확인할 수 있습니다.
-      초대하기 버튼을 선택해 프로젝트에 새로운 사용자를 초대할 수 있습니다.
-      프로젝트를 생성한 사용자는 내보내기 버튼을 통해 사용자를 프로젝트에서 내보낼 수 있습니다.`,
-  },
-  {
-    key: "북마크 모달",
-    content: `URL을 입력해 북마크를 등록할 수 있습니다.
-      북마크는 프로젝트 단위로 공유되기 때문에, 프로젝트에 참여중인 모든 사용자가 확인하고 추가하거나 제거할 수 있습니다.`,
-  },
-  {
-    key: "프로필 모달",
-    content:
-      "프로필 이미지, 사용자 이름, 사용자 소개 글의 편집 및 로그아웃이 가능합니다.",
-  },
-];
-
-export default function TutorialText({ tutorialTarget }: Props) {
-  const target =
-    tutorialTarget === "List" ? TUTORIAL_LIST_TEXT : TUTORIAL_PROJECT_TEXT;
+export default function TutorialText({
+  textPerPage,
+  totalText,
+  paginate,
+}: Props) {
+  const pageNumbers = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 1; i <= Math.ceil(totalText / textPerPage); i++) {
+    pageNumbers.push(i);
+  }
   return (
-    <TutorialTextLayout>
-      {target.map((singleElement: any) => (
-        <TutorialTextContent key={singleElement.key}>
-          <TutorialTextParagraph>{singleElement.key}</TutorialTextParagraph>
-          {singleElement.content}
-        </TutorialTextContent>
-      ))}
-    </TutorialTextLayout>
+    <div>
+      <nav>
+        <TutorialUl>
+          {pageNumbers.map((number: any) => (
+            <TutorialLi key={number}>
+              <TutorialSpan onClick={() => paginate(number)}>
+                {number}
+              </TutorialSpan>
+            </TutorialLi>
+          ))}
+        </TutorialUl>
+      </nav>
+    </div>
   );
 }
