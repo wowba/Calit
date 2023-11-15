6bb6e0068c56f891310a7b583e569778bad5d09d
applevalley <skatkd7571@naver.com>
2023-11-15 15:53:39 +0900

    design: 튜토리얼 데이터 속성명 수정

--- src/components/modal/TutorialModal.tsx
+++ src/components/modal/TutorialModal.tsx
@@ -4,7 +4,6 @@ import CommonPaginationLayout from "../layout/CommonPaginationLayout";
 
 const TutorialTextContent = styled.div`
   white-space: pre-line;
-  max-height: 15rem;
   overflow: scroll;
 
   &::-webkit-scrollbar {
@@ -77,7 +76,7 @@ const ModalContainer = styled.div`
 const ModalText = styled.div`
   background: white;
   width: 400px;
-  min-height: 15rem;
+  min-height: 20rem;
   padding: 20px;
   border-radius: 10px;
   text-align: center;
@@ -91,40 +90,43 @@ const ModalText = styled.div`
 `;
 
 const TUTORIAL_LIST_TEXT = [
-  { title: "Calit?", text: "세상에서 가장 쉬운 협업 툴 Calit!" },
+  { key: "Calit?", content: "세상에서 가장 쉬운 협업 툴 Calit!" },
   {
-    title: "프로젝트 만들기",
-    text: "하단의 + 버튼을 통해 프로젝트 추가가 가능합니다.",
+    key: "프로젝트 만들기",
+    content: "하단의 + 버튼을 통해 프로젝트 추가가 가능합니다.",
   },
   {
-    title: "프로젝트 공유하기",
-    text: `프로젝트 카드 우측 상단의 클립보드 아이콘을 선택해 해당 프로젝트의 주소를 공유할 수 있습니다.`,
+    key: "프로젝트 공유하기",
+    content: `프로젝트 카드 우측 상단의 클립보드 아이콘을 선택해 해당 프로젝트의 주소를 공유할 수 있습니다.`,
   },
   {
-    title: "프로젝트 삭제하기",
-    text: "자신이 생성한 프로젝트인 경우, 프로젝트 카드 우측 상단의 톱니바퀴 아이콘을 선택해 프로젝트의 배경 이미지를 변경하거나, 프로젝트를 삭제할 수 있습니다.",
+    key: "프로젝트 삭제하기",
+    content:
+      "자신이 생성한 프로젝트인 경우, 프로젝트 카드 우측 상단의 톱니바퀴 아이콘을 선택해 프로젝트의 배경 이미지를 변경하거나, 프로젝트를 삭제할 수 있습니다.",
   },
   {
-    title: "프로젝트 참여하기",
-    text: "공유받은 링크를 주소창에 바로 입력하거나, 초대링크로 입장 버튼을 클릭한 뒤 붙여넣어 프로젝트에 참여할 수 있습니다.",
+    key: "프로젝트 참여하기",
+    content:
+      "공유받은 링크를 주소창에 바로 입력하거나, 초대링크로 입장 버튼을 클릭한 뒤 붙여넣어 프로젝트에 참여할 수 있습니다.",
   },
 ];
 
 const TUTORIAL_PROJECT_TEXT = [
   {
-    title: "프로젝트 멤버 모달",
-    text: `프로젝트에 참여중인 사용자들의 정보를 확인할 수 있습니다.
+    key: "프로젝트 멤버 모달",
+    content: `프로젝트에 참여중인 사용자들의 정보를 확인할 수 있습니다.
       초대하기 버튼을 선택해 프로젝트에 새로운 사용자를 초대할 수 있습니다.
       프로젝트를 생성한 사용자는 내보내기 버튼을 통해 사용자를 프로젝트에서 내보낼 수 있습니다.`,
   },
   {
-    title: "북마크 모달",
-    text: `URL을 입력해 북마크를 등록할 수 있습니다.
+    key: "북마크 모달",
+    content: `URL을 입력해 북마크를 등록할 수 있습니다.
       북마크는 프로젝트 단위로 공유되기 때문에, 프로젝트에 참여중인 모든 사용자가 확인하고 추가하거나 제거할 수 있습니다.`,
   },
   {
-    title: "프로필 모달",
-    text: "프로필 이미지, 사용자 이름, 사용자 소개 글의 편집 및 로그아웃이 가능합니다.",
+    key: "프로필 모달",
+    content:
+      "프로필 이미지, 사용자 이름, 사용자 소개 글의 편집 및 로그아웃이 가능합니다.",
   },
 ];
 
@@ -154,9 +156,11 @@ export default function Tutorial({ isShowTutorial, setIsShowTutorial }: Props) {
     <ModalContainer>
       <ModalText>
         {posts.slice(offset, page).map((singleElement: any) => (
-          <TutorialTextContent key={singleElement.title}>
-            <TutorialTextTitle>{singleElement.title}</TutorialTextTitle>
-            <TutorialTextParagraph>{singleElement.text}</TutorialTextParagraph>
+          <TutorialTextContent key={singleElement.key}>
+            <TutorialTextTitle>{singleElement.key}</TutorialTextTitle>
+            <TutorialTextParagraph>
+              {singleElement.content}
+            </TutorialTextParagraph>
           </TutorialTextContent>
         ))}
         <TutorialButtonBox>
