cde60ca6b726f29c9fc38bdf6c1f9fa045a5f2f9
applevalley <skatkd7571@naver.com>
2023-11-16 10:11:42 +0900

    design: 사이드바 이미지 그림자 추가

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -15,26 +15,40 @@ const SidebarLayout = styled.div`
   width: 15rem;
   height: 100%;
 
-  padding: 4rem 1.25rem 1.25rem 1.25rem;
+  padding: 4rem 0.5rem 1.25rem 0.5rem;
 `;
 
 const ProjectInfoBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+  box-shadow: inset 0px 7rem 3rem -5rem rgba(255, 255, 255, 1);
 `;
 
 const ProjectTitleParagraph = styled.p`
   font-size: 1.5rem;
   font-weight: 900;
-  margin: 0.25rem 0 2.8rem 0;
+  margin-left: 0.25rem;
 `;
 
+const ProjectTitleBox = styled.div`
+  display: flex;
+  margin: 0.5rem 0 1.5rem 0;
+`;
+
+const ProjectTitleBoxLabel = styled.div`
+  width: 0.5rem;
+  height: auto;
+  background-color: #7064ff;
+  border-radius: 9px;
+  margin-right: 0.5rem;
+`;
 const ProjectProfileImg = styled.img`
   width: 100%;
   height: 7rem;
   object-fit: cover;
   border-radius: 0.5rem;
+  z-index: -1;
 `;
 
 export default function Sidebar() {
@@ -51,8 +65,11 @@ export default function Sidebar() {
             }
             alt="Project Profile Img"
           />
-          <ProjectTitleParagraph>{projectData.name}</ProjectTitleParagraph>
         </ProjectInfoBox>
+        <ProjectTitleBox>
+          <ProjectTitleBoxLabel />
+          <ProjectTitleParagraph>{projectData.name}</ProjectTitleParagraph>
+        </ProjectTitleBox>
         <RecentKanban />
       </div>
     </SidebarLayout>
