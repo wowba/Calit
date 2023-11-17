44761401d0aa4c93f89c8cf06884f914f57acaa5
wowba <leeyuwk54@gmail.com>
2023-11-17 13:25:58 +0900

    폰트 변경

--- src/index.tsx
+++ src/index.tsx
@@ -18,16 +18,17 @@ const GlobalStyle = createGlobalStyle`
   table {border-collapse:collapse;border-spacing:0}
 
   /* font */
-  * {
+  /* * {
     @font-face {
     font-family: 'Roboto';
     src: url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap') format('ttf');
     font-weight: 400;
     }
-  }
+  } */
   * {
       box-sizing: border-box;
-      font-family: 'Roboto';
+      font-family: 'Noto Sans KR', sans-serif;
+      /* font-family: 'Roboto'; */
   }
 `;
 
